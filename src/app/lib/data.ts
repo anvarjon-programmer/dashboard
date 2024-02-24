359ecec4148c6587f3b9a7bb5185f886a1560b3c
Anvarjon <143145991+anvarjon-programmer@users.noreply.github.com>
2024-02-24 14:44:14 +0500

    pagination

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,5 +1,5 @@
 import { pages } from "next/dist/build/templates/app-page";
-import {User} from "./models";
+import {Product, User} from "./models";
 import{connectToDB} from './utels';
 import { IUserPromise } from "../dashboard/types/users";
 // import{IProductPromise} from "../types/products"
@@ -12,6 +12,19 @@ export const fetchUser = async(q:string,page:number):Promise<IUserPromise | unde
         connectToDB()
         const count = await User.find({username:{$regex: regex}}).countDocuments()
         const users = await User.find({username:{$regex: regex}}).limit(ITEM_PER_PAGE).skip(ITEM_PER_PAGE * (page - 1))
+        return {count,users}
+    }catch(error){
+        console.log(error);
+        
+    }
+}
+export const fetchProducts = async(q:string,page:number):Promise<IUserPromise | undefined>=>{
+    const regex = new RegExp(q, "i")
+    const ITEM_PER_PAGE = 2;
+    try{
+        connectToDB()
+        const count = await Product.find({title:{$regex: regex}}).countDocuments()
+        const users = await Product.find({title:{$regex: regex}}).limit(ITEM_PER_PAGE).skip(ITEM_PER_PAGE * (page - 1))
         return {count,users}
     }catch(error){
         console.log(error);
